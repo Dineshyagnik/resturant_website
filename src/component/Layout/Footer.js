9f75569c979a39541f0b76b397c8197d9b02fcab
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-05-14 11:53:45 +0530

    footer added

--- src/component/Layout/Footer.js
+++ src/component/Layout/Footer.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = () => {
   return (
-    <div>
-        <h1>Footer Component</h1>
-    </div>
+    <>
+      <Box sx={{ textAlign: 'center', bgcolor: '#1A1A19', color: 'white', p: 3 }}>
+        <Box sx={{
+          my: 3, "& svg": {
+            fontSize: '60px',
+            cursor: 'pointer',
+            mr: 2
+          },
+          "& svg:hover": {
+            color: 'goldenrod',
+            transform: 'translateX(5px)',
+            transition: 'all 400ms',
+          }
+
+        }}>
+          <InstagramIcon />
+          <TwitterIcon />
+          <FacebookIcon />
+          <YouTubeIcon />
+        </Box>
+        <Typography variant='h5' sx={{
+          "@media (max-width:600px)": {
+            fontSize: '1rem'
+          }
+        }}>
+          All Rights Reserved &copy; Resturant WEB
+        </Typography>
+      </Box>
+    </>
   )
 }
 
-export default Footer
+export default Footer;
