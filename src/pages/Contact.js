b42754942ae1608e23194fe5aa6d4a0d1502e276
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-05-15 22:13:39 +0530

    contact added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Layout from '../component/Layout/Layout';
-import { Box, Paper, Table, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import MailIcon from '@mui/icons-material/Mail';
+import CallIcon from '@mui/icons-material/Call';
+import { 
+  Box, 
+  Paper, 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableContainer, 
+  TableHead, 
+  TableRow, 
+  Typography,
+
+} from '@mui/material';
 
 const Contact = () => {
   return (
     <Layout>
-      <Box sx={{my:10,ml:10, "& h4":{fontWeight:'bold',mb:2}}}>
+      <Box sx={{my:5,ml:10, "& h4":{fontWeight:'bold',mb:2}}}>
         <Typography variant='h4'>
           Contact My Resturant
         </Typography>
@@ -19,14 +33,33 @@ const Contact = () => {
           But because of the change and the experience of some new varieties, I became a lot more encouraged to eat in restaurants. One of my most favorite restaurants is the tomato Indian restaurant. They can always surprise me and offer one of those new meals or offer a modern sauce that I like.
         </p>
       </Box>
-      <Box>
+      <Box sx={{m:3,width:'600px',ml:10,"@media(max-width:600px)":{
+        width:'300px'
+      }}}>
         <TableContainer component={Paper}>
           <Table aria-label='contact table'>
             <TableHead>
               <TableRow>
-                <TableCell>Contact Details</TableCell>
+                <TableCell sx={{bgcolor:'black',color:'white'}} align='center'>Contact Details</TableCell>
               </TableRow>
             </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                <SupportAgentIcon sx={{color:'red',pt:1}}/> 1800 23000 123 (toll-free)
+                </TableCell>
+              </TableRow>
+              <TableRow>
+              <TableCell>
+                <MailIcon sx={{color:'skyblue',pt:1}}/> help@myrest.com
+                </TableCell>
+              </TableRow>
+              <TableRow>
+              <TableCell>
+                <CallIcon sx={{color:'green',pt:1}}/> +99-123456789 (toll-free)
+                </TableCell>
+              </TableRow>
+            </TableBody>
           </Table>
 
         </TableContainer>
